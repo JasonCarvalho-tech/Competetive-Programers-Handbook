d7915ddce037e76f8ea2a8ef6115a8155fe6a431
jason <jml123carvalho@gmail.com>
2021-04-02 20:41:10 +0530

    initial commit

---
+++ Part 1/Chapter 1/C++-code-template.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	//solution goes here
+}
+//compile this code with 'g++ -std=c++11 -Wall C++-code-template.cpp -o -C++-code-template'
