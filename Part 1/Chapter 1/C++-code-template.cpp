9532055a4663f846f550d02de328dfd977e688f9
jason <jml123carvalho@gmail.com>
2021-04-03 15:41:36 +0530

    removed extra -

--- Part 1/Chapter 1/C++-code-template.cpp
+++ Part 1/Chapter 1/C++-code-template.cpp
@@ -5,4 +5,4 @@ using namespace std;
 int main() {
 	//solution goes here
 }
-//compile this code with 'g++ -std=c++11 -Wall C++-code-template.cpp -o -C++-code-template'
+//compile this code with 'g++ -std=c++11 -Wall C++-code-template.cpp -o C++-code-template'
