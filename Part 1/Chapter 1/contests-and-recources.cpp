765fd68838dffe0ce7dc0e1ace9a2f6b590dfc33
jason <jml123carvalho@gmail.com>
2021-04-11 09:47:41 +0530

    initial commit

---
+++ Part 1/Chapter 1/contests-and-recources.cpp
@@ -0,0 +1,23 @@
+/*
+ 	International Olympiad in Informatics (IOI) (for secondary school students): https://ioinformatics.org/
+	International Collegiate Programming Contest (ICPC): https://icpc.global/
+	Online Contests
+		Codeforces: https://codeforces.com/
+		AtCoder: https://atcoder.jp/
+		CS Academy(seems to be inactive): https://csacademy.com/contests/
+		HackerRank: https://www.hackerrank.com/domains/cpp
+		Topcoder: https://www.topcoder.com/community/arena 
+		Facebook Hacker Cup: https://www.facebook.com/codingcompetitions/hacker-cup/
+		Google Code Jam: https://codingcompetitions.withgoogle.com/codejam
+		Yandex.Algorithm: https://yandex.com/cup/algorithm/
+
+	Books
+		Programming Challenges: The Programming Contest Training Manual, ISBN: 9780387220819
+		Competitive Programming 4: The New Lower Bound of Programming Contests in the 2020s, https://cpbook.net/
+		The Ultimate Problem Set from the University of Warsaw Programming Comptetitions, http://www.lookingforachallengethebook.com/uploads/1/4/5/5/14555448/preview-_looking_for_a_challenge.pdf
+
+		Introduction to Algorithms, https://en.wikipedia.org/wiki/Introduction_to_Algorithms
+		Algorithm Design, ISBN: 9780132131087
+		The Algorithm Design Manual, ISBN: 9783030542566
+*/
+
---
+++ Part 1/Chapter 1/mathematics.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	/*
+		Sum formulas: http://www.columbia.edu/itc/sipa/math/summation.html
+			arithmetic progression: https://saylordotorg.github.io/text_intermediate-algebra/s12-02-arithmetic-sequences-and-serie.html
+			geometric progressions: https://saylordotorg.github.io/text_intermediate-algebra/s12-03-geometric-sequences-and-series.html
+				harmonic sum: https://www.math.drexel.edu/~tolya/123_harmonic.pdf
+
+		Set theory: refer book
+		Logic: https://www.cpp.edu/~ftang/courses/CS130/notes/logic%20basics.pdf
+			Predicates and Quantifiers: https://www.cpp.edu/~ftang/courses/CS130/notes/predicate%20logic.pdf
+		
+		Functions: refer book
+
+		Logarithms:	http://www.columbia.edu/itc/sipa/math/logarithms.html
+		
+	*/
+}
